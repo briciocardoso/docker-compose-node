7366e3b04552f4268b12bb215c0cd503a7862a9e
Bricio Cardoso <bricio@donorbox.org>
2023-04-10 23:29:17 -0300

    Add name list

--- node/index.js
+++ node/index.js
@@ -29,8 +29,51 @@ function createUniqueName() {
   return uniqueNamesGenerator(generatorConfig);
 }
 
+function getPeople() {
+  const connection = createConnection();
+
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM people`, (error, result) => {
+      if (error) {
+        console.log(`Erro. Retorno do resolve getPeople(): ${error}`);
+        return reject(error);
+      }
+      console.log(`Sucesso. Retorno dentro do resolve getPeople(): ${result}`);
+      return resolve(result);
+    });
+  })
+}
+
+function insertPeople() {
+  const connection = createConnection();
+  const name = createUniqueName();
+  const sql = `INSERT INTO people(name) values('${name}')`;
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (error, result) => {
+      if (error) {
+        console.log(`Erro. Retorno do resolve insertPeople(): ${error}`);
+        return reject(error);
+      }
+      console.log(`Sucesso. Retorno do resolve insertPeople(): ${result}`);
+      return resolve(result);
+    })
+  })
+}
+
 app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle Rocks!!</h1>');
+  insertPeople().then(() => {
+    getPeople().then((people) => {
+      let content = '<h1>Full Cycle Rocks!!</h1>'
+      content += '<table>'
+      content += '<h4>Lista de nomes cadastrada no banco de dados:</h4>'
+      for (const person of people) {
+        content += `<tr><td>${person.id}. ${person.name}</td></tr>`
+      }
+      content += '</table>'
+      res.send(content);
+      console.log(`Dados do banco: ${people}`)
+    });
+  });
 })
 
 app.listen(port, () => {
